a72e6c74b54c57155558f81519ef3086b08807c2
Yuri Matos <yurimatos40@gmail.com>
2023-06-18 22:10:15 -0300

    Optimizing requests to AWS SDK

--- src/server.js
+++ src/server.js
@@ -1,3 +1,15 @@
 const app = require('./app');
+const getAPIKeyFromParameterStore = require('./aws/AwsCalls');
+
+async function getParams() {
+    try {
+      const apiKey = await getAPIKeyFromParameterStore();
+      process.env.MAPBOX_ACCESS_TOKEN = apiKey;
+    } catch (error) {
+      console.error('Error starting the server:', error);
+    }
+}
+
+getParams();
 
 app.listen(3000, () => console.log('running node app teste'));
